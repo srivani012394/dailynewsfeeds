d6286cc63dd64d973fdc0ae80bb799c459668298
akhil-gautam <akhilg@mindfiresolutions.com>
2018-12-23 16:27:10 +0530

    Done: * initial commit * added migration for keyword and seeded the keywords table

---
+++ db/data/20181223102553_add_keyword.rb
@@ -0,0 +1,14 @@
+class AddKeyword < SeedMigration::Migration
+  TECHS = ["rails", "devops", "cloud", "ai", "ruby", "react.js", "android"]
+  def up
+    TECHS.each do |tech|
+      record = Keyword.new(name: tech)
+      record.save
+    end
+  end
+
+  def down
+    Keyword.destroy_all
+    puts "Keywords deleted successfully!"
+  end
+end
