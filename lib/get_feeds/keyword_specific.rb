2e7c3f8d81256ffd1a843c1ccd907aefba0a46aa
akhil-gautam <akhilg@mindfiresolutions.com>
2019-01-02 10:47:35 +0530

    Done: * changed the feeds * added condition on whom to send the mails
     On branch master
     Changes to be committed:
    	new file:   app/views/feed_mailer/_hackernews_feed.html.haml
    	new file:   app/views/feed_mailer/_quora_feed.html.haml
    	new file:   app/views/feed_mailer/_reddit_feed.html.haml
    	deleted:    app/views/feed_mailer/daily_feed.html.erb
    	new file:   app/views/feed_mailer/daily_feed.html.haml
    	modified:   lib/get_feeds/keyword_specific.rb
    	modified:   lib/send_mails/feeds_mailer.rb

--- lib/get_feeds/keyword_specific.rb
+++ lib/get_feeds/keyword_specific.rb
@@ -3,9 +3,15 @@ module GetFeeds
     class << self
       def news_for_user(email)
         keywords = User.where(email: email).last.keywords
-        feeds = []
+        feeds = {
+          hackernews: [],
+          quora: [],
+          reddit: []
+        }
         keywords.each do|keyword|
-          feeds << {topic: keyword.name, news: keyword.hackernews_feeds}
+          feeds[:hackernews] << { topic: keyword.name, news: keyword.hackernews_feeds }
+          feeds[:quora] << { topic: keyword.name, news: keyword.quora_posts }
+          feeds[:reddit] << { topic: keyword.name, news: keyword.reddit_feeds }
         end
         feeds
       end
--- lib/send_mails/feeds_mailer.rb
+++ lib/send_mails/feeds_mailer.rb
@@ -3,7 +3,7 @@ TODAY = Date.today
 module SendMails
   class FeedsMailer
     def self.send_in_batches
-      User.where.not(last_feed_on: TODAY).find_each(batch_size: 100) do|user|
+      User.where.not(last_feed_on: TODAY, confirmed_at: nil).find_each(batch_size: 100) do|user|
         FeedMailer.daily_feed(user.email).deliver_later
         user.update(last_feed_on: TODAY)
       end
