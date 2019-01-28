6b7e5d7333311a7e76019e16035134b68e4df08a
vanisree <vimala.vani123@gmail.com>
2019-01-28 21:20:58 +0530

    production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,6 +99,9 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.default_url_options = { host: 'https://dailynewsfeeds.herokuapp.com' }
+
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
