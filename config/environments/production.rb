a0d27a26f3a4e33be0eef9592796ee223c69f88b
vanisree <vimala.vani123@gmail.com>
2019-01-28 21:49:00 +0530

    fix the smtp configuration for gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server
   # for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -104,15 +104,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
 
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
+    domain: "https://dailynewsfeeds.herokuapp.com",
     user_name:  ENV['GMAIL_USER_ID'],
     password: ENV['GMAIL_PWD'],
     authentication: 'plain',
     enable_starttls_auto: true
   }  
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
 end
