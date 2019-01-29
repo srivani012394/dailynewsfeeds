239fe0b7cb447f328e5ed9b3ce0d884daa0b9879
vanisree <vimala.vani123@gmail.com>
2019-01-29 12:10:34 +0530

    production env sendgrid added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,6 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server
   # for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -106,10 +105,11 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 
   ActionMailer::Base.smtp_settings = {
-    user_name:  ENV['GMAIL_USER_ID'],
-    password: ENV['GMAIL_PWD'],
+    user_name:  ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
     domain: 'gmail.com',
     address: 'smtp.sendgrid.net',
     port: 587,
